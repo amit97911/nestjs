23d1de53969e9f65e86f41b05c2ce942a478d932
Amit Patel <amit97911@gmail.com>
2022-08-26 00:14:02 +0530

    Response status code

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,10 +1,14 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import { response } from 'express';
 
 @Controller('coffees')
 export class CoffeesController {
     @Get()
-    findAll(){
-        return "Returning all coffees";
+    // findAll(){
+    //     return "Returning all coffees";
+    // }
+    findAll(@Res() response){
+        return response.status(200).send("Returning all coffees");
     }
 
     @Get(':id')
@@ -16,6 +20,7 @@ export class CoffeesController {
     }
 
     @Post()
+    @HttpCode(HttpStatus.GONE)
     create(@Body() body){
         return body;
     }
