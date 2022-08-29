94bc34e745faede81f84b2bc20e6694a1cab4b7b
Amit Patel <amit97911@gmail.com>
2022-08-30 00:24:45 +0530

    Added TypeORM entity

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,16 @@
-export class Coffee{
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('coffee') //by default the TypeORM will use the table name as the class name in small case.
+export class Coffee {
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     name: string;
+
+    @Column()
     brand: string;
+
+    @Column('json', { nullable: true })
     flavors: string[];
 }
